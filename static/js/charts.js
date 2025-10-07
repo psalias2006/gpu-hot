f0bbc684bb7e9c9100f680db17eca3f1ce88eae0
psalias <psalias2006@hotmail.com>
2025-10-08 02:55:15 +0300

    Feature/add time axes (#4)
    * add time
    
    * add time
    
    ---------
    
    Co-authored-by: Panos <>

--- static/js/charts.js
+++ static/js/charts.js
@@ -47,7 +47,18 @@ const chartConfigs = {
             },
             scales: {
                 x: {
-                    display: false
+                    display: true,
+                    grid: {
+                        display: false,
+                        drawBorder: false
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.55)',
+                        font: { size: 12 },
+                        maxRotation: 0,
+                        autoSkip: true,
+                        maxTicksLimit: 6
+                    }
                 },
                 y: {
                     min: 0,
@@ -160,7 +171,18 @@ const chartConfigs = {
             },
             scales: {
                 x: {
-                    display: false
+                    display: true,
+                    grid: {
+                        display: false,
+                        drawBorder: false
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.55)',
+                        font: { size: 12 },
+                        maxRotation: 0,
+                        autoSkip: true,
+                        maxTicksLimit: 6
+                    }
                 },
                 y: {
                     min: 0,
@@ -263,7 +285,18 @@ const chartConfigs = {
             },
             scales: {
                 x: {
-                    display: false
+                    display: true,
+                    grid: {
+                        display: false,
+                        drawBorder: false
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.55)',
+                        font: { size: 12 },
+                        maxRotation: 0,
+                        autoSkip: true,
+                        maxTicksLimit: 6
+                    }
                 },
                 y: {
                     min: 0,
@@ -356,7 +389,18 @@ const chartConfigs = {
             },
             scales: {
                 x: {
-                    display: false
+                    display: true,
+                    grid: {
+                        display: false,
+                        drawBorder: false
+                    },
+                    ticks: {
+                        color: 'rgba(255, 255, 255, 0.55)',
+                        font: { size: 12 },
+                        maxRotation: 0,
+                        autoSkip: true,
+                        maxTicksLimit: 6
+                    }
                 },
                 y: {
                     min: 0,
